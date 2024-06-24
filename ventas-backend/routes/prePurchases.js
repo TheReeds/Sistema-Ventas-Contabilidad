52602d9cdb0ed6a4f8491a30fbb2af110f9de007
TheReeds <george159357852@gmail.com>
2024-06-24 12:06:39 -0500

    update

--- ventas-backend/routes/prePurchases.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPrePurchase, getAllPrePurchases, getPrePurchaseById } = require('../controllers/prePurchases');
-
-router.post('/', createPrePurchase);
-router.get('/', getAllPrePurchases);
-router.get('/:id', getPrePurchaseById);
-
-module.exports = router;
--- ventas-backend/routes/sales.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createSale, getAllSales, generateSalesReport } = require('../controllers/sales');
-
-router.post('/', createSale);
-router.get('/', getAllSales);
-router.get('/report', generateSalesReport);
-
-module.exports = router;
